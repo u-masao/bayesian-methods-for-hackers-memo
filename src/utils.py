7943db9bd23ff6cfd48e9f0269e88d5d8c7069a0
u-masao <4973920+u-masao@users.noreply.github.com>
2023-05-30 23:51:50 +0900

    [add] utility functions

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import os
+import pickle
+from pathlib import Path
+
+
+def savefig(fig, save_path_string: str) -> None:
+    """
+    figure を保存する
+    """
+    save_path = Path(save_path_string)
+    os.makedirs(save_path.parent, exist_ok=True)
+    fig.savefig(save_path)
+
+
+def load_trace_and_model(model_path: str):
+    # load trace and model
+    with open(model_path, "rb") as fo:
+        trace, model = pickle.load(fo)
+    return trace, model
+
+
+def save_trace_and_model(trace, model, model_path: str):
+    # save trace and model
+    with open(model_path, "wb") as fo:
+        pickle.dump((trace, model), fo)
