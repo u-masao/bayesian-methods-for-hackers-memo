b527b8d9e283b2a4837d2f7718b26a2fd81a57e9
u-masao <4973920+u-masao@users.noreply.github.com>
2023-05-30 23:35:44 +0900

    [update] implement two bernoulli model

--- src/chapter02/two_bernoulli.py
+++ src/chapter02/two_bernoulli.py
@@ -67,5 +67,5 @@ def main(**kwargs):
 
 if __name__ == "__main__":
     log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.In_aFO, format=log_fmt)
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
     main()
