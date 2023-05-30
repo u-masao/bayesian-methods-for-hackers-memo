b928983205c940c6c822a0bfb6a201be6b269db1
u-masao <4973920+u-masao@users.noreply.github.com>
2023-05-31 00:04:44 +0900

    [update] remove obs. code refact.

--- src/chapter02/two_bernoulli.py
+++ src/chapter02/two_bernoulli.py
@@ -24,6 +24,46 @@ def log_metrics(occurences, p_true, label):
     logger.info(f"{label} metrics: \n{metrics}")
 
 
+def sampling(occurences_a, occurences_b):
+    model = pm.Model()
+    with model:
+        p_a = pm.Uniform("p_a", 0, 1)
+        p_b = pm.Uniform("p_b", 0, 1)
+        delta = pm.Deterministic("delta", p_a - p_b)  # noqa: F841
+        obs_a = pm.Bernoulli("obs_a", p_a, observed=occurences_a)  # noqa: F841
+        obs_b = pm.Bernoulli("obs_b", p_b, observed=occurences_b)  # noqa: F841
+        step = pm.Metropolis()
+        trace = pm.sample(20000, step=step)
+        burned_trace = trace[1000:]
+
+    return burned_trace, model
+
+
+def plot_histogram(p_a_true, p_b_true, burned_trace):
+    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+    ax.vlines(p_a_true, 0, 90, linestyle="--", label="true $p_A$ (unknown)")
+    ax.vlines(p_b_true, 0, 90, linestyle="--", label="true $p_B$ (unknown)")
+    ax.hist(
+        burned_trace["p_a"],
+        bins=25,
+        density=True,
+        label="$p_a$ dist.",
+        alpha=0.5,
+    )
+    ax.hist(
+        burned_trace["p_b"],
+        bins=25,
+        density=True,
+        label="$p_b$ dist.",
+        alpha=0.5,
+    )
+
+    ax.legend()
+    ax.grid()
+    fig.suptitle("$p_A$ と $p_B$ の事後分布と真の値")
+    return fig
+
+
 @click.command()
 @click.option(
     "--figure_dir", type=click.Path(), default="reports/figures/chapter02/"
@@ -45,55 +85,17 @@ def main(**kwargs):
     log_metrics(occurences_a, p_a_true, "a")
     log_metrics(occurences_b, p_b_true, "b")
 
-    model = pm.Model()
-    with model:
-        p_a = pm.Uniform("p_a", 0, 1)
-        p_b = pm.Uniform("p_b", 0, 1)
-        delta = pm.Deterministic("delta", p_a - p_b)  # noqa: F841
-        obs_a = pm.Bernoulli("obs_a", p_a, observed=occurences_a)  # noqa: F841
-        obs_b = pm.Bernoulli("obs_b", p_b, observed=occurences_b)  # noqa: F841
-        step = pm.Metropolis()
-        trace = pm.sample(20000, step=step)
-        burned_trace = trace[1000:]
-
+    trace, model = sampling(occurences_a, occurences_b)
     save_trace_and_model(trace, model, kwargs["model_output_filepath"])
 
-    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-    ax.vlines(p_a_true, 0, 90, linestyle="--", label="true $p_A$ (unknown)")
-    ax.vlines(p_b_true, 0, 90, linestyle="--", label="true $p_B$ (unknown)")
-    ax.hist(
-        burned_trace["p_a"],
-        bins=25,
-        density=True,
-        label="$p_a$ dist.",
-        alpha=0.5,
+    savefig(
+        plot_histogram(
+            p_a_true,
+            p_b_true,
+            trace,
+        ),
+        Path(kwargs["figure_dir"]) / "bernoulli.png",
     )
-    ax.hist(
-        burned_trace["p_b"],
-        bins=25,
-        density=True,
-        label="$p_b$ dist.",
-        alpha=0.5,
-    )
-    ax.hist(
-        occurences_a,
-        density=True,
-        bins=25,
-        label="$occurences_a$",
-        alpha=0.5,
-    )
-    ax.hist(
-        occurences_b,
-        density=True,
-        bins=25,
-        label="$occurences_b$",
-        alpha=0.5,
-    )
-
-    ax.legend()
-    ax.grid()
-    fig.suptitle("$p_A$ と $p_B$ の事後分布と真の値")
-    savefig(fig, Path(kwargs["figure_dir"]) / "bernoulli.png")
 
 
 if __name__ == "__main__":
